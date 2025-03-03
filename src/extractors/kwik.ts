cd525fa293f9f2b5ba07fc726391e1db67e61101
Durgesh Kumar Dwivedi <durgeshdwivedi81@gmail.com>
2025-03-03 18:27:08 +0530

    test 4

--- src/extractors/kwik.ts
+++ src/extractors/kwik.ts
@@ -14,7 +14,8 @@ class Kwik extends VideoExtractor {
           authority: 'animepahe.ru',
           accept: 'application/json, text/javascript, */*; q=0.01',
           'accept-language': 'en-US,en;q=0.9',
-          cookie: '__ddg2_=;',
+          Cookie:
+            'cf_clearance=ojHYv3Fxoc7piqPhKETH850zEO3WJ9ts9XZRHkpXQMY-1741006506-1.2.1.1-UuIE3nqXc48mpmapcSbCdO5iYLIsAQ6RuuY1HsX.NG0lh0fhHULx5.PonkLmPKvg0_2nNTFkyDL8znI3BptsYot03wfcNORhiqro3iPq_O9NaeIlROCSQ7VKh7jYy8iiIyR0XERPCsybmQ9mNBp5sxdSs1CVooQxL6GXIhTj6v4IZZ9LAiHCL59nsxtKB46IPVIIwIcW.a5S0B70y7nmmsKQIJCyqMOi9CWnHK1GgbqGSL9jm.2VxsqsaXTxAbqYL3Y3wa_fyw1d3xPmiG3kNLZRoQf38L9KZ_fOouTqwyb8HTs_GBR_22NojpAovuybqXPYdVlWAX3YdK6LD2Qn32mRfm9sQjduukEYRRzzA4F1dUawt4TNqYMEIU0SNjLuWVcv7Tf6gYcufJ2HxMMLEoITcBrmVtBMAkDW5EIXtPs',
           dnt: '1',
           'sec-ch-ua': '"Not A(Brand";v="99", "Microsoft Edge";v="121", "Chromium";v="121"',
           'sec-ch-ua-mobile': '?0',
